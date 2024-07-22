4b6b906ca1700791e207b53fc340ead389b1e184
ay-josiah <155204698+ay-josiah@users.noreply.github.com>
2024-07-22 12:40:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ global.caption = process.env.CAPTION || global.caption || "ᴘᴏᴡᴇʀᴇᴅ 
 
 
 global.devs = "923184474176" // Developer Contact
-global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null,2348061413577";
+global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null,2348061413577,2348089895884";
 global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "2349137920622";
 
 
